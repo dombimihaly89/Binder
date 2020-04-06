aacc22257be65922011803414afd383cdbfee126
Mihaly Dombi <dombimihaly89@gmail.com>
2020-04-06 12:55:30 +0200

    First commit

---
+++ binder-backend/src/main/java/hu/flowacademy/binder/Binder.java
@@ -0,0 +1,13 @@
+package hu.flowacademy.binder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Binder {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Binder.class, args);
+	}
+
+}
---
+++ binder-backend/src/test/java/hu/flowacademy/binder/BinderTests.java
@@ -0,0 +1,13 @@
+package hu.flowacademy.binder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinderTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
